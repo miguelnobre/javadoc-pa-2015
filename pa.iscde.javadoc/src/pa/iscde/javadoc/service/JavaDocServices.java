402b935a1b111754420afa89d07bbec2b3ea1d76
Jose Pedro de Sousa Martins <zpedro.martins@gmail.com>
2016-01-08 10:27:41 +0000

    Added Interface Comments.

--- pa.iscde.javadoc/src/pa/iscde/javadoc/service/JavaDocServices.java
+++ pa.iscde.javadoc/src/pa/iscde/javadoc/service/JavaDocServices.java
@@ -2,15 +2,58 @@ package pa.iscde.javadoc.service;
 
 import java.io.File;
 
+/**
+ * Interfaces that specifies the Services provided by the JavaDoc Plugin. The
+ * JavaDoc Plugin provides, as a service, a method that allows other PIDESCO
+ * plugins to request on-demand the generation the Javadoc for a specified file
+ * or for a specific method or field contained in specified file.
+ * 
+ * @author jpmartins
+ *
+ */
 public interface JavaDocServices {
-    
+
+    /**
+     * Types of elements to generate javadoc to:
+     * 
+     * <li>{@link METHOD}</li>
+     * <li>{@link FIELD}</li>
+     *
+     */
     enum Type {
+	/**
+	 * Type Method
+	 */
 	METHOD,
+	/**
+	 * Type Method
+	 */
 	FIELD
     };
-    
+
+    /**
+     * Returns a {@link String} containing the generated javadoc for the
+     * specified file.
+     * 
+     * @param file
+     *            name of the file for which the will be generated.
+     * @return the generated javadoc.
+     */
     String render(File file);
-    
+
+    /**
+     * Returns a {@link String} containing the generated javadoc for the method
+     * / field contained in the specified file
+     * 
+     * @param file
+     *            name of the file for which javadoc the will be generated
+     * @param {@link
+     * 	   Type} of the element specified by the field <code>name</code>
+     * @param name
+     *            of the field or method for which should the javadoc be
+     *            generated
+     * @return the generated javadoc
+     */
     String render(File file, Type type, String name);
 
 }
